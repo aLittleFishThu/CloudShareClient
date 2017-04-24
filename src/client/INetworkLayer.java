9b7ea801481ecd264cf19cc5327cdcb0537c2f29
yzj <yuzhujun14@hotmail.com>
2017-04-24 17:48:21 +0800

    add uploadFile 加入上传文件功能

--- src/client/INetworkLayer.java
+++ src/client/INetworkLayer.java
@@ -1,11 +1,14 @@
 package client;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.apache.http.client.ClientProtocolException;
 
 import common.ChangePasswdResult;
+import common.CloudFile;
 import common.Credential;
+import common.FileResult;
 import common.LoginResult;
 import common.RegisterResult;
 
@@ -22,5 +25,7 @@ public interface INetworkLayer {
 	public RegisterResult register(Credential cred) 
 			throws ClientProtocolException, IOException;
 	public ChangePasswdResult changePasswd(String password, String newPassword)
+			throws ClientProtocolException, IOException;
+	public FileResult uploadFile(CloudFile cloudFile,File file)
 			throws ClientProtocolException, IOException;
 }
--- src/client/NetworkLayer.java
+++ src/client/NetworkLayer.java
@@ -1,10 +1,13 @@
 package client;
 
+import java.io.File;
 import java.io.IOException;
 
 import common.ChangePasswdResult;
+import common.CloudFile;
 import common.Convert;
 import common.Credential;
+import common.FileResult;
 import common.LoginResult;
 import common.RegisterResult;
 
@@ -13,6 +16,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
+import org.apache.http.entity.FileEntity;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -122,6 +126,7 @@ public class NetworkLayer implements INetworkLayer{
 		HttpEntity requestBody=
 				new StringEntity(jsonRequest.toString(), ContentType.APPLICATION_JSON);
 		httpPost.setEntity(requestBody);              //加入请求Body，ContentType自动设置
+		
 		/**
 		 * 获得响应并解析
 		 */
@@ -140,7 +145,43 @@ public class NetworkLayer implements INetworkLayer{
 		 * 关闭客户端并返回结果给上层
 		 */
 		httpClient.close();										//关闭服务器
-		return Convert.toChangePasswdResult(status);			//返回登录结果
+		return Convert.toChangePasswdResult(status);			//返回登录结果	
+	}
+	
+	public FileResult uploadFile(CloudFile cloudFile,File file) 
+			throws ClientProtocolException, IOException{
+		/**
+		 * 创建客户端
+		 */
+		CloseableHttpClient httpClient=HttpClients.createDefault(); //创建HttpClient
+		String filename=cloudFile.getFilename();
+		HttpPost httpPost = new HttpPost(serverUri+"/file?filename="+filename);   		
+																	//Post方法
+		/**
+		 * 设置发送内容
+		 */
+		httpPost.addHeader("Cookie", cookie);   		//Cookie加入Header里
+		HttpEntity requestBody=new FileEntity(file,ContentType.DEFAULT_BINARY);
+														//文件加入requestBody
+		httpPost.setEntity(requestBody);              //加入请求Body，ContentType自动设置
+		/**
+		 * 获得响应并解析
+		 */
+		HttpResponse httpResponse=httpClient.execute(httpPost); //发出请求并获得响应
+		int statusCode=httpResponse.getStatusLine().getStatusCode();
+		if (statusCode==403)
+			return FileResult.unAuthorized;      		//判断响应码
+		else if (statusCode!=200)
+			return FileResult.unknownError;
+		HttpEntity responseEntity=httpResponse.getEntity();    	//获得Entity
+		String responseBody=Convert.toString(responseEntity.getContent());//获得Body
 		
+		JSONObject jsonResponse=new JSONObject(responseBody);   //转为JSON对象
+		String status=jsonResponse.getString("status");         //获取状态
+		/**
+		 * 关闭客户端并返回结果给上层
+		 */
+		httpClient.close();										//关闭服务器
+		return Convert.toFileResult(status);			//返回登录结果			
 	}
 }
